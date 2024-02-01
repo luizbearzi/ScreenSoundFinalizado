b524b9a89e5a33c30d817be99f9dd1cb024403f5
Luiz <Luiz@DESKTOP-KT5UBSK>
2024-02-01 00:06:50 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Menus/MenuRegistrarBanda.cs
@@ -0,0 +1,33 @@
+﻿using OpenAI_API;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuRegistrarBanda : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        base.Executar(bandasRegistradas);
+        //ExibirTituloDaOpcao("Registro das bandas");
+        Console.Write("Digite o nome da banda que deseja registrar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+        Banda banda = new Banda(nomeDaBanda);
+        bandasRegistradas.Add(nomeDaBanda, banda);
+
+        // Integrando CHat GPT
+        var client = new OpenAIAPI("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"); // Criando o objeto para client de OpenAIAPI
+                                                                                           // esse objeto client vai requisitar e fazer pedidos para esta plataforma, está API da OpenAI
+
+        var chat = client.Chat.CreateConversation();
+
+        chat.AppendSystemMessage($"Resuma a banda {nomeDaBanda} em 1 parágrafo. Adote um estilo informal.");
+
+        string resposta = chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
+        banda.Resumo = resposta;
+
+        Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+        Console.WriteLine("\nDigite uma tecla para votar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
---
+++ ScreenSound/Modelos/Avaliacao.cs
@@ -0,0 +1,17 @@
+﻿namespace ScreenSound.Modelos;
+
+internal class Avaliacao
+{
+    //Estabele os limites mínimos e máximos para a nota
+    public Avaliacao(int nota)
+    {
+        Nota = nota;
+    }
+
+    public int Nota { get; }
+    public static Avaliacao Parse(string texto)
+    {
+        int nota = int.Parse(texto);
+        return new Avaliacao(nota);
+    }
+}
